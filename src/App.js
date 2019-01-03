7a614130fc1a554091f37a04813805642a4b4f19
zhoucumt <553817478@qq.com>
2019-01-03 19:19:50 +0800

    add iconfont

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
+                        <i className="iconfont icon-clock"></i>
                     </p>
                     <a
                         className="App-link"
