7f704965fb760d4bd63fde7b376676fcfbd938c9
zhoucumt <553817478@qq.com>
2019-01-31 09:21:32 +0800

    add svg

--- src/views/Content/About/index.js
+++ src/views/Content/About/index.js
@@ -6,10 +6,23 @@ class About extends Component {
         return (
             <div style={{fontSize: '50px'}} className="about-page">
                 <div>About</div>
+
                 <svg width="300" height="180">
                     <circle cx="30"  cy="50" r="25" />
-                    <circle cx="90"  cy="50" r="25" class="red" />
-                    <circle cx="150" cy="50" r="25" class="fancy" />
+                    <circle cx="90"  cy="50" r="25" className="red" />
+                    <circle cx="150" cy="50" r="25" className="fancy" stroke="red" />
+                </svg>
+
+                <svg width="300" height="180">
+                    <line x1="0" y1="0" x2="200" y2="0" className="line" />
+                </svg>
+
+                <svg width="300" height="180">
+                    <polyline points="3,3 30,28 3,53" fill="none" stroke="black" />
+                </svg>
+
+                <svg width="300" height="180">
+                    <rect x="0" y="0" height="100" width="200" />
                 </svg>
             </div>
         );
