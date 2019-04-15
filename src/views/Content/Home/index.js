6b8adf4ad65658e4c7fa976d9fcaaba6ab832d1e
zhoucumt <553817478@qq.com>
2019-04-15 14:07:49 +0800

    change

--- src/views/Content/Home/index.js
+++ src/views/Content/Home/index.js
@@ -3,11 +3,35 @@ import api from '@/api/index.js';
 import {Button} from 'antd';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        // 创建 ref 存储 textInput DOM 元素
+        this.textInput = React.createRef();
+        this.focusTextInput = this.focusTextInput.bind(this);
+    }
+
+    focusTextInput() {
+        // 直接使用原生 API 使 text 输入框获得焦点
+        // 注意：通过 "current" 取得 DOM 节点
+        this.textInput.current.focus();
+    }
+
     render () {
         return (
             <div style={{fontSize: '50px'}}>
                 <h3>Home</h3>
                 <Button>按钮</Button>
+                <div>
+                    <input
+                        type="text"
+                        ref={this.textInput} />
+
+                    <input
+                        type="button"
+                        value="Focus the text input"
+                        onClick={this.focusTextInput}
+                    />
+                </div>
             </div>
         );
     }
