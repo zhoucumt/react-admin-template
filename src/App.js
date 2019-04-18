845aea5251ae9604b23406c8b3d839dfdc093b67
zhoucumt <553817478@qq.com>
2019-04-18 10:20:34 +0800

    change

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {BrowserRouter as Router} from "react-router-dom";
 import {renderRoutes} from "react-router-config";
 // 引入视图
 import RouterLink from "./views/RouterLink";
-import ContnetRouter from "./views/Content/router";
+import ContentRouter from "./views/Content/router";
 
 import './App.less';
 
 // 合并路由
-const routes = [...ContnetRouter];
+const routes = [...ContentRouter];
 
 class App extends Component {
     render() {
